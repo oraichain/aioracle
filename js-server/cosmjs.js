e7f13156de56847e4b7032d63b79b61c95d353cb
ducphamle2 <ducphamle212@gmail.com>
2021-12-15 18:40:48 +0700

    add cosmjs to test contract

---
+++ js-server/cosmjs.js
@@ -0,0 +1,36 @@
+const { DirectSecp256k1HdWallet } = require("@cosmjs/proto-signing");
+const { stringToPath } = require("@cosmjs/crypto");
+const cosmwasm = require('@cosmjs/cosmwasm-stargate');
+const { GasPrice } = require('@cosmjs/cosmwasm-stargate/node_modules/@cosmjs/stargate/build');
+
+const network = {
+    rpc: process.env.NETWORK_RPC || "https://testnet-rpc.orai.io",
+    prefix: "orai",
+}
+
+const collectWallet = async (mnemonic) => {
+    const wallet = await DirectSecp256k1HdWallet.fromMnemonic(
+        mnemonic,
+        {
+            hdPaths: [stringToPath("m/44'/118'/0'/0/0")],
+            prefix: network.prefix,
+        }
+    );
+    return wallet;
+}
+
+const execute = async ({ mnemonic, address, handleMsg, memo, amount, gasData: { gasAmount, denom } }) => {
+    try {
+        const wallet = await collectWallet(mnemonic);
+        const [firstAccount] = await wallet.getAccounts();
+        const client = await cosmwasm.SigningCosmWasmClient.connectWithSigner(network.rpc, wallet, { gasPrice: { gasData } ? GasPrice.fromString(`${gasAmount}${denom}`) : undefined, prefix: network.prefix });
+        const input = JSON.parse(handleMsg);
+        const result = await client.execute(firstAccount.address, address, input, memo, amount);
+        return result.transactionHash;
+    } catch (error) {
+        console.log("error in executing contract: ", error);
+        throw error;
+    }
+}
+
+export { execute };
--- js-server/index.js
+++ js-server/index.js
@@ -6,6 +6,8 @@ const {
   verifyHexProof
 } = require('./merkle-proof-tree');
 const level = require('level');
+const { execute } = require('./cosmjs');
+require('dotenv').config();
 
 const express = require('express')
 const app = express()
@@ -37,44 +39,55 @@ app.get('/get_proof', async (req, res) => {
 })
 
 app.post('/submit_report', (req, res) => {
-  let report = req.body;
-  console.log("report: ", report);
-  if (!whiteList.includes(report.executor)) return res.send({ code: 401 })
-  let countKey = `${report.request_id}_report_count`;
-  let requestIdString = report.request_id.toString();
-  db.get(requestIdString, async (error, value) => {
-    let reports = [];
-    // if we cant find the request id, we init new
-    if (error) {
-      reports = [report];
-    } else {
-      reports = JSON.parse(value);
-      // otherwise we append into the existing value
-      reports.push(report);
-    }
-    await db.put(requestIdString, JSON.stringify(reports));
-    let count = 1;
-    try {
-      // increment count
-      count = await db.get(countKey);
-      count++;
-    } catch (error) {
-    }
-    await db.put(countKey, count.toString());
-    if (count >= threshold) {
-      console.log("reports: ", reports);
-      // form a merkle root based on the value
-      const values = reports.map(JSON.stringify);
-      const leaves = values.map((value) => sha256(value).toString('hex'));
+  // let report = req.body;
+  // console.log("report: ", report);
+  // if (!whiteList.includes(report.executor)) return res.send({ code: 401 })
+  // let countKey = `${report.request_id}_report_count`;
+  // let requestIdString = report.request_id.toString();
+  // db.get(requestIdString, async (error, value) => {
+  //   let reports = [];
+  //   // if we cant find the request id, we init new
+  //   if (error) {
+  //     reports = [report];
+  //   } else {
+  //     reports = JSON.parse(value);
+  //     // otherwise we append into the existing value
+  //     reports.push(report);
+  //   }
+  //   await db.put(requestIdString, JSON.stringify(reports));
+  //   let count = 1;
+  //   try {
+  //     // increment count
+  //     count = await db.get(countKey);
+  //     count++;
+  //   } catch (error) {
+  //   }
+  //   await db.put(countKey, count.toString());
+  //   if (count >= threshold) {
+  //     console.log("reports: ", reports);
+  //     // form a merkle root based on the value
+  //     const values = reports.map(JSON.stringify);
+  //     const leaves = values.map((value) => sha256(value).toString('hex'));
 
-      const tree = new MerkleProofTree(leaves);
-      await db.put(tree.getRoot(), JSON.stringify(leaves));
-      await Promise.all(values.map((value, i) => db.put(leaves[i], value)));
+  //     const tree = new MerkleProofTree(leaves);
 
-      console.log('save data in', tree.getHexRoot());
-    }
-    return res.send({ code: 200 });
-  })
+  //     // store the leaves to retrieve later. Can possibly store this on contract (but could be expensive)
+  //     await db.put(tree.getRoot(), JSON.stringify(leaves));
+  //     await Promise.all(values.map((value, i) => db.put(leaves[i], value)));
+
+  //     console.log('save data in', tree.getHexRoot());
+
+  //     // store the merkle root on-chain
+  //     const executeResult = await execute({ mnemonic: process.env.MNEMONIC, address: process.env.CONTRACT_ADDRESS, handleMsg: JSON.stringify({ register_merkle_root: { merkle_root: tree.getHexRoot() } }), gasAmount: { amount: "0", denom: "orai" } });
+
+  //     console.log("execute result: ", executeResult);
+  //   }
+  //   return res.send({ code: 200 });
+  // })
+
+  const executeResult = await execute({ mnemonic: process.env.MNEMONIC, address: process.env.CONTRACT_ADDRESS, handleMsg: JSON.stringify({ register_merkle_root: { merkle_root: tree.getHexRoot() } }), gasAmount: { amount: "0", denom: "orai" } });
+
+  console.log("execute result: ", executeResult);
 })
 
 app.listen(port, () => {
