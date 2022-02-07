7fc5e9bf111073a604f67fc8ca384a826179b3f3
ducphamle2 <ducphamle212@gmail.com>
2022-02-07 15:51:35 +0700

    change oraiwasmjs to npm version

--- js-server/models/oraiwasm.js
+++ js-server/models/oraiwasm.js
@@ -1,4 +1,4 @@
-const OraiwasmJs = require('@oraichain/oraiwasm-js').default;
+const OraiwasmJs = require('@oraichain2/oraiwasm-js').default;
 
 const oraiwasmJs = new OraiwasmJs(process.env.URL || 'https://testnet-lcd.orai.io', process.env.CHAIN_ID || 'Oraichain-testnet');
 oraiwasmJs.setBech32MainPrefix('orai');
