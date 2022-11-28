acf94771fc1a303a079a645b302a5b429a7944c3
ducphamle2 <ducphamle212@gmail.com>
2022-11-27 19:31:57 -0800

    fix ping execute error

--- js/utils.js
+++ js/utils.js
@@ -42,7 +42,6 @@ const getStageInfo = async (contractAddr) => {
     })
 
     const data = await queryWasmRaw(contractAddr, input);
-    console.log("data: ", data)
     if (!data.data) {
         throw "No request to handle";
     }
