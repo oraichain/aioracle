be5eaba27690188ee685b1b26daa07b5c15e44d8
Pham Tu <tubackkhoa@gmail.com>
2023-05-13 19:38:11 +0700

    using codegen

--- packages/contracts-build/index.ts
+++ packages/contracts-build/index.ts
@@ -1,7 +1,16 @@
+import { SigningCosmWasmClient } from '@cosmjs/cosmwasm-stargate';
+// this is for polyfill implementation
+import { SimulateCosmWasmClient } from '@terran-one/cw-simulate';
 import path from 'path';
 
 const contractDir = path.join(path.dirname(module.filename), 'data');
 
-export const getContractDir = (name: 'aioracle-contract' = 'aioracle-contract') => {
-  return path.join(contractDir, name + '.wasm');
+export type ContractName = 'aioracle-contract';
+
+export const getContractDir = (contractName: ContractName = 'aioracle-contract') => {
+  return path.join(contractDir, contractName + '.wasm');
+};
+
+export const deployContract = async <T>(client: SigningCosmWasmClient | SimulateCosmWasmClient, senderAddress: string, msg: T, label: string, contractName?: ContractName) => {
+  return await client.deploy(senderAddress, getContractDir(contractName), msg, label, 'auto');
 };
