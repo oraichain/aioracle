6704a0f97e14eeda910bc3cf166d4db4dcc7267b
ducphamle2 <ducphamle212@gmail.com>
2023-08-10 09:41:01 +0700

    refactored code

--- packages/executor/src/utils/script-execute.ts
+++ packages/executor/src/utils/script-execute.ts
@@ -1,52 +1,93 @@
-import { spawn } from 'child_process';
-import { AioracleContractClient, Service, TestCaseState } from '@oraichain/aioracle-contracts-sdk/src';
-import { spawnPromise } from './common';
+import {
+  AioracleContractClient,
+  Service,
+  TestCaseState,
+} from "@oraichain/aioracle-contracts-sdk/src";
+import { spawnPromise } from "./common";
 
 type AssertResponse = {
   dsource: string;
   testCaseResult: string;
-}
+};
 
 type HandleScriptResponse = {
   aggregateResponse: string;
   assertResults: AssertResponse[][];
   dsourceResults: string[];
-}
+};
 
 type DenoScriptResponse = {
   code: number;
   data: string;
-  error: string
-}
+  error: string;
+};
 
 function convertToDenoScriptInput(params: string[]): string {
-  return Buffer.from(JSON.stringify(params)).toString('base64');
+  return Buffer.from(JSON.stringify(params)).toString("base64");
 }
 
-export const executeService = async (
+export async function executeService(
   requestId: number,
   serviceName: string,
   requestInput: string,
   aioracleClient: AioracleContractClient
-) => {
+) {
   const serviceInfo = await aioracleClient.getService({ serviceName });
   console.log("request input is: ", requestInput);
   let { aggregateResponse, assertResults, dsourceResults } = await handleScript(
     serviceInfo.service,
     requestInput
   );
-  let serviceResult: string = Buffer.from(JSON.stringify({ aggregateResponse, assertResults, dsourceResults })).toString('base64');
+  let serviceResult: string = Buffer.from(
+    JSON.stringify({ aggregateResponse, assertResults, dsourceResults })
+  ).toString("base64");
   // return request id so that in the callback we can collect it
   return {
     serviceResult,
-    requestId
+    requestId,
   };
 }
 
-const executeTestCases = async (
+export async function handleScript(
+  service: Service,
+  requestInput: string
+): Promise<HandleScriptResponse> {
+  const { oscript_url, dsources, tcases } = service;
+  // execute the data sources
+  const dsourceResults: string[] = [];
+  let assertResults: AssertResponse[][] = [[]];
+  // let validDSources = [];
+  for (let dsource of dsources) {
+    // if any test case status is true & data source status is false => the data source is invalid
+    let testCaseResults = await executeTestCases(dsource.script_url, tcases);
+    assertResults.push(testCaseResults);
+
+    // By default, the user's input is the last element of the parameter list
+    let result = await processDenoScript(
+      dsource.script_url,
+      convertToDenoScriptInput([...dsource.parameters, requestInput])
+    );
+    console.log("dsource result: ", result);
+    dsourceResults.push(result.trim()); // trim to remove trailing space & newline char
+    // validDSources.push(dsource);
+  }
+
+  // aggregate results
+  let aggregateResponse = await processDenoScript(
+    oscript_url,
+    convertToDenoScriptInput(dsourceResults)
+  );
+  return {
+    aggregateResponse: aggregateResponse.trim(),
+    assertResults,
+    dsourceResults,
+  };
+}
+
+async function executeTestCases(
   dsourceScriptUrl: string,
   testCaseStates: TestCaseState[]
-): Promise<AssertResponse[]> => {
+): Promise<AssertResponse[]> {
   // run data source given the parameters of the test cases
   let assertResults = new Array<AssertResponse>();
   for (let testCase of testCaseStates) {
@@ -61,45 +102,17 @@ const executeTestCases = async (
   return assertResults;
 }
 
-export const handleScript = async (service: Service, requestInput: string): Promise<HandleScriptResponse> => {
-  const { oscript_url, dsources, tcases } = service;
-  // execute the data sources
-  const dsourceResults: string[] = [];
-  let assertResults: AssertResponse[][] = [[]];
-  // let validDSources = [];
-  for (let dsource of dsources) {
-    // if any test case status is true & data source status is false => the data source is invalid
-    let testCaseResults = await executeTestCases(dsource.script_url, tcases)
-    assertResults.push(testCaseResults)
-
-    // By default, the user's input is the last element of the parameter list
-    let result = await processDenoScript(
-      dsource.script_url,
-      convertToDenoScriptInput([...dsource.parameters, requestInput])
-    );
-    console.log("dsource result: ", result);
-    dsourceResults.push(result.trim()); // trim to remove trailing space & newline char
-    // validDSources.push(dsource);
-  }
-
-  // aggregate results
-  let aggregateResponse = await processDenoScript(oscript_url, convertToDenoScriptInput(dsourceResults));
-  return {
-    aggregateResponse: aggregateResponse.trim(),
-    assertResults,
-    dsourceResults,
-  };
-}
-
-const processDenoScript = async (scriptUrl: string, params: string): Promise<string> => {
-  const result = await spawnPromise('deno', [
-    'run',
-    '--allow-net',
-    '--unstable',
+async function processDenoScript(
+  scriptUrl: string,
+  params: string
+): Promise<string> {
+  const result = (await spawnPromise("deno", [
+    "run",
+    "--allow-net",
+    "--unstable",
     scriptUrl,
-    params
-  ]) as DenoScriptResponse;
-  if (result.error.length > 0 || result.code !== 0)
-    return result.error;
+    params,
+  ])) as DenoScriptResponse;
+  if (result.error.length > 0 || result.code !== 0) return result.error;
   return result.data;
 }
--- packages/executor/src/utils/ws.ts
+++ packages/executor/src/utils/ws.ts
@@ -1,22 +1,24 @@
-import * as WebSocket from 'ws';
-import config from '../config';
-import { logError } from './logs';
-import { processRequest } from './process-request';
+import * as WebSocket from "ws";
+import config from "../config";
+import { logError } from "./logs";
+import { processRequest } from "./process-request";
 
 export const wsClientConnect = (mnemonic: string) => {
-  const ws = new WebSocket(`${config.WEBSOCKET_URL}/websocket`, { handshakeTimeout: 10000 });
-  ws.on('open', function open() {
+  const ws = new WebSocket(`${config.WEBSOCKET_URL}/websocket`, {
+    handshakeTimeout: 10000,
+  });
+  ws.on("open", function open() {
     ws.send(
       JSON.stringify({
-        jsonrpc: '2.0',
-        method: 'subscribe',
+        jsonrpc: "2.0",
+        method: "subscribe",
         params: [`tm.event='Tx' AND wasm.action = 'handle_request'`],
-        id: 1
+        id: 1,
       })
     );
   });
 
-  ws.on('message', async function incoming(data: any) {
+  ws.on("message", async function incoming(data: any) {
     try {
       const result = JSON.parse(data).result;
       if (
@@ -26,28 +28,32 @@ export const wsClientConnect = (mnemonic: string) => {
         return;
       }
       const events = result.events;
-      console.log('events: ', events);
-      const requestId = events['wasm.stage'];
-      console.log('request id: ', requestId);
-      console.log('event contract addr: ', events['wasm._contract_address'][0]);
-      if (events['execute._contract_address'][0] === config.CONTRACT_ADDRESS) {
+      console.log("events: ", events);
+      const requestId = events["wasm.stage"];
+      console.log("request id: ", requestId);
+      console.log("event contract addr: ", events["wasm._contract_address"][0]);
+      console.log("contract addr: ", config.CONTRACT_ADDRESS);
+      if (events["execute._contract_address"][0] === config.CONTRACT_ADDRESS) {
         // optional await or running background, no need to pass param
         processRequest(parseInt(requestId), mnemonic);
       }
     } catch (error) {
-      logError(error, 'error socket message');
+      logError(error, "error socket message");
     }
   });
 
-  ws.on('error', (error) => {
-    logError(error, 'error socket on ');
+  ws.on("error", (error) => {
+    logError(error, "error socket on ");
     ws.close(1005, JSON.stringify(error));
   });
 
-  ws.on('close', (error) => {
-    console.error('\x1b[31m%s\x1b[0m', 'on close error code: ', error);
+  ws.on("close", (error) => {
+    console.error("\x1b[31m%s\x1b[0m", "on close error code: ", error);
     setTimeout(function () {
-      console.log('\x1b[32m%s\x1b[0m', '\nThe Oracle Runner is reconnecting ...\n');
+      console.log(
+        "\x1b[32m%s\x1b[0m",
+        "\nThe Oracle Runner is reconnecting ...\n"
+      );
       wsClientConnect(mnemonic);
     }, 5000);
   });
